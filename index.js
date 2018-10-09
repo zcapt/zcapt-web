54054e968e30383473c295586fac5d0256b04770
plbin97 <i@teenet.me>
2018-10-09 11:20:43 -0400

    test

---
+++ index.js
@@ -0,0 +1,6 @@
+import  "./src/sass/test.scss";
+let a = ()=> {
+	document.write("It works");
+};
+a();
+
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+        }]
+    }
+};
+
