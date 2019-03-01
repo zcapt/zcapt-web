4fc089c7e036681d21b8f7712981bbae85c5f85b
Linbin <i@teenet.me>
2019-02-28 20:14:20 -0500

    new

--- test/connGenerater.js
+++
@@ -1,8 +0,0 @@
-let Base64 = require('js-base64').Base64;
-let a = {
-    init: "https://api.thatseed.org/captcha/newCaptcha",
-    large: "https://api.thatseed.org/captcha/getImage",
-    small: "https://api.thatseed.org/captcha/getSmallImage",
-    veri: "https://api.thatseed.org/captcha/verify"
-};
-console.log(Base64.encode(JSON.stringify(a)));
--- test/test.js
+++ test/test.js
@@ -1,11 +1,14 @@
-let i = 0;
-j();
-function j() {
-    if (i<20) {
-        console.log(i);
-        i ++;
-        setTimeout(() => {
-            j();
-        },1000);
-    }
-}
+let initia = require('../src/request/initialize');
+global.XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+let assert = require('assert');
+
+describe("Initialize Test", () => {
+    it("Communicate with Server",(done) => {
+        initia.init("https://api.thatseed.org/captcha/newCaptcha",(result) => {
+            if (result === null) {
+                assert.fail("Error")
+            }
+            done();
+        })
+    })
+});
